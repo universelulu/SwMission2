be61a63a44d86668d9d701697a6ce66094a284d2
guraudrk <lee1066515@naver.com>
2024-06-16 13:32:42 +0900

    new commit(2024-06-16)-조원 이장한 커밋(search_revision 브랜치 병합,함수 수정)

---
+++ finalMission2/src/finalMission2/BookSearch.java
@@ -0,0 +1,37 @@
+package finalMission2;
+
+import java.util.Arrays;
+
+public class BookSearch {
+   
+   public static Book search_bs(Book[] books, int BookId)
+   {
+      Arrays.sort(books,(b1,b2) -> Integer.compare(b1.GetId(), b2.GetId())); // 이진 탐색을 위해 id를 기준으로 오름차순 정렬
+      int left= 0;
+      int right= books.length-1;
+      
+      while(left<=right)
+      {
+         int midIndex = (right-left) / 2;
+         
+         if(books[midIndex].GetId() == BookId)
+         {
+            return books[midIndex];
+         }
+         
+         else if(books[midIndex].GetId() < BookId)
+         {
+            left++;
+         }
+         
+         else
+         {
+            right--;
+         }
+      }
+      
+      return null; // 찾지 못하였으면 null 값 반환
+   }
+
+}
+
