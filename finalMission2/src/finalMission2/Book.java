a6ee81903260e6a3ccc2ca1222a1a2485fa130e8
jangs <jangs@DESKTOP-R46BTDF>
2024-06-16 13:03:50 +0900

    first commit

---
+++ finalMission2/src/finalMission2/Book.java
@@ -0,0 +1,29 @@
+package finalMission2;
+
+public class Book {
+	
+	private int id;
+	private String title;
+	
+	public Book(int id, String title) 
+	{
+		this.id = id;
+		this.title = title;
+	}
+	
+	public int GetId() 
+	{
+		return id;
+	}
+	
+	public String GetTitle()
+	{
+		return title;
+	}
+	
+	public String toString() 
+	{
+		return "{id: "+ id + ", title: " + title + "}";
+	}
+
+}
